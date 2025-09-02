82beb08bf8d36dd2e23387fa7e620fd0186f20e8
kallyas <kallyasmedia@gmail.com>
2025-09-02 20:44:07 +0300

    feat: Register FlutterTts and SpeechToText plugins for macOS
    This commit adds the registration of the `FlutterTtsPlugin` and
    `SpeechToTextMacosPlugin` to the `GeneratedPluginRegistrant.swift` file.
    
    This ensures that the Text-to-Speech and Speech-to-Text functionalities
    are properly initialized and available for use within the Flutter macOS
    application.  These plugins were not automatically registered previously.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,13 +6,17 @@ import FlutterMacOS
 import Foundation
 
 import connectivity_plus
+import flutter_tts
 import path_provider_foundation
 import share_plus
 import shared_preferences_foundation
+import speech_to_text_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
+  FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
 }
